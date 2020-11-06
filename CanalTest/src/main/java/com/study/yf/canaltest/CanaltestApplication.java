a71e66fb8d906d3d3648085bee226bafc0413bf1
yangfei <yangfei@cennavi.com.cn>
2020-11-06 13:47:46 +0800

    增加Canal学习测试项目 修改Kafka配置环境

---
+++ CanalTest/src/main/java/com/study/yf/canaltest/CanaltestApplication.java
@@ -0,0 +1,13 @@
+package com.study.yf.canaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanaltestApplication.class, args);
+    }
+
+}
---
+++ CanalTest/src/test/java/com/study/yf/canaltest/CanaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.yf.canaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
